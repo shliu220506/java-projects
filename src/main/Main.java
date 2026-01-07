71186c201394fe5eb78287ac90a3f89cc0b97cda
shliu220506 <30719166+shliu220506@users.noreply.github.com>
2026-01-07 16:40:24 +0800

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,64 @@
-# JAVA file content
+package com.trust.ncsrxinchuang.util;
 
-import java.io.File
+import java.security.MessageDigest;
 
-class FileUtils {
+public class Main
+{
+	private static String byteArrayToHexString(byte b[])
+	{
+		StringBuffer resultSb = new StringBuffer();
+		for (int i = 0; i < b.length; i++)
+			resultSb.append(byteToHexString(b[i]));
+
+		return resultSb.toString();
+	}
+
+	private static String byteToHexString(byte b)
+	{
+		int n = b;
+		if (n < 0)
+			n += 256;
+		int d1 = n / 16;
+		int d2 = n % 16;
+		return hexDigits[d1] + hexDigits[d2];
+	}
+
+	public static String encode(String str)
+	{
+		return encode(str, "UTF-8");
+	}
+
+	public static String encode(String str, String charsetname)
+	{
+		String resultString = null;
+		try
+		{
+			resultString = new String(str);
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// 使用指定的字节更新摘要
+			//md.update(str.getBytes("utf-8"));
+			if (charsetname == null || "".equals(charsetname))
+			{
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+			}
+			else
+			{
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+			}
+		}
+		catch (Exception exception)
+		{
+			
+		}
+		return resultString.toUpperCase();
+	}
+
+	private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d",
+			"e", "f" };
+
+	public void main(String[] args){
+		Main bootstrap = new Main();
+		bootstrap.encode("aa");
+	}
 }
+
